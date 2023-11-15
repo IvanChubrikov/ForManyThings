859e4e9a5003716d31a7669fed88ef3d84359210
ichubrikov <ichubrikov.mail>
2023-11-15 20:44:35 +0300

    change

---
+++ First-nginxProvision/variables.tf
@@ -0,0 +1,24 @@
+variable "private_key_path" {
+  type    = string
+  default = "~/.ssh/OTUS-NEW"
+}
+
+variable "public_key" {
+  type    = string
+  default = ""
+}
+
+variable "auth_token" {
+  type    = string
+  default = ""
+}
+
+variable "cloud_id_variable" {
+  type    = string
+  default = "b1gi0vt5112j4o86dsog"
+}
+
+variable "folder_id_variable" {
+  type    = string
+  default = "b1gomou4r7kh74k6b5g1"
+}
